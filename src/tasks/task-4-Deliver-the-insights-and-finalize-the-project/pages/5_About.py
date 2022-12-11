d979ff2cce912b0ced4547fdcb27397dd968eec5
MightyStud <73258591+MightyStud@users.noreply.github.com>
2022-12-11 12:47:16 +0200

    edit about page

--- src/tasks/task-4-Deliver-the-insights-and-finalize-the-project/pages/5_About.py
+++ src/tasks/task-4-Deliver-the-insights-and-finalize-the-project/pages/5_About.py
@@ -6,8 +6,8 @@ st.markdown(
     """
     This code was written for educational purposes as a part of [Omdena challenge](https://omdena.com/chapter-challenges/high-school-thanaweya-amma-data-analysis/).
     The dataset used was collected in a collaborative fashion and anonymized. The code of this 
-    streamlit app is available [here](https://github.com/MightyStud/High-School-Data-Analysis-Streamlit-App).
+    streamlit app is available [here](https://github.com/OmdenaAI/giza-chapter-high-school-final-exams/tree/main/src/tasks/task-4-Deliver-the-insights-and-finalize-the-project).
 
-    If you have any issues or concerns contact me on [LinkedIn](https://www.linkedin.com/in/mohamedahmedx2/)
+    If you have any issues or concerns contact omdena chapter leader on [LinkedIn](https://www.linkedin.com/in/mohamedahmedx2/)
 """
 )
